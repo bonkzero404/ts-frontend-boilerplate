273132af10e0b4bb4de6460723f212e1c26787c1
Janitra Panji <panji@Janitras-MacBook-Air.local>
2019-11-25 22:32:11 +0700

    declare module image

--- src/components/Common/Common.tsx
+++ src/components/Common/Common.tsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import { hot } from 'react-hot-loader/root';
 import { State } from '../../store/reducers';
 import * as Style from './Style.scss';
+import ImageLogo from '../../assets/images/logo.png';
 
 type TProps = ReturnType<typeof mapStateToProps>;
 
@@ -11,7 +12,7 @@ const Common: React.FunctionComponent<TProps> = (props) => {
   return (
     <div className={Style.container}>
       <div className={Style.box}>
-        <img className={Style.logo} src={require('../../assets/images/logo.png')} />
+        <img className={Style.logo} src={ImageLogo} />
         <h1 className={Style.headText}>React JS</h1>
         <p>Horay <Link to="/counter">Click Me</Link> to play counter</p>
         <p>You are here : {props.pathname}</p>
