25d23ecf3ef04b7c3f17abdcc8b15f024cd32de2
Janitra Panji <panji@Janitras-MacBook-Air.local>
2020-04-20 07:06:11 +0700

    remove webpack path

--- webpack/webpack-path.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-
-const path = require('path');
-const root = path.resolve(__dirname, '..');
-
-module.exports = {
-  src: path.join(root, process.env.SOURCE_DEV),
-  out: path.join(root, process.env.BUILD_DEST),
-};
