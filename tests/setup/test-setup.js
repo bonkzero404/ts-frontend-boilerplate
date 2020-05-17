d6550b331694519cdfaacf26716addf1dca82a6f
Janitra Panji <bonkzero404@gmail.com>
2020-05-17 21:04:27 +0700

    remove window.react

--- tests/setup/test-setup.js
+++ tests/setup/test-setup.js
@@ -1,7 +1,6 @@
 import Enzyme, { shallow, render, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
-window.React = require('react');
 // React 16 Enzyme adapter
 Enzyme.configure({ adapter: new Adapter() });
 export { shallow, mount, render };
